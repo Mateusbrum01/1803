a2085961ff051934ba8e5a92898cc0c2063c222a
Mateusbrum01 <mateus_cagneti@estudante.sesisenai.org.br>
2025-03-18 09:15:58 -0300

    first commit

---
+++ ex01/script.js
@@ -0,0 +1,10 @@
+
+function verfificarPar(numero) {
+    if (numero % 2 === 0) {
+        console .log("Número par");
+    } else {
+        console .log("Número ímpar");
+    }
+}
+
+verfificarPar(7)
