90199a6bc0ea03d7f4a965ddae1467bf96541f0a
Mateusbrum01 <mateus_cagneti@estudante.sesisenai.org.br>
2025-03-18 10:05:44 -0300

    ADD - ex02 e 01

---
+++ ex02/script.js
@@ -0,0 +1,15 @@
+
+function operacao(numero1, numero2) {
+
+  const soma = numero1 + numero2;
+  const subtracao = numero1 - numero2;
+  const multiplicacao = numero1 * numero2;
+  const divisao = numero2 % numero1;
+
+  console .log (soma)
+  console .log (subtracao)
+  console .log (multiplicacao)
+  console .log (divisao)
+
+}
+  console .log(operacao(20, 2))
